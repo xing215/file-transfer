3ddef3aa63e8f4fc84f890d26b9849ce66d54010
xing215 <dev@xing512.onmicrosoft.com>
2024-07-20 14:40:23 +0700

    Add return type

--- file_splitter.py
+++ file_splitter.py
@@ -1,6 +1,6 @@
 import os
 
-def split_file(filepath, chunk_maxsize=1048576):
+def split_file(filepath, chunk_maxsize=1048576) -> int:
     part_num = 0
     with open(filepath, 'rb') as file:
         while True:
@@ -12,7 +12,7 @@ def split_file(filepath, chunk_maxsize=1048576):
             part_num += 1
     return part_num
 
-def merge_file(filepath, part_num):
+def merge_file(filepath, part_num) -> bool:
     with open(filepath, 'wb') as output_file:
         for i in range(part_num):
             part_filepath = f"{filepath}.part{i}"
