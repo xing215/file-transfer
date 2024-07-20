c1cb862597f8b4bc39de14a70055a836beaacf89
xing215 <dev@xing512.onmicrosoft.com>
2024-07-20 14:48:43 +0700

    Template

--- file_transfer.py
+++ file_transfer.py
@@ -1,7 +1,7 @@
 import socket
 import file_splitter
 
-def send(send_socket : socket, filepath : str) -> bool:
+def send(send_socket : socket, filepath : str, BUFFER_SIZE : int = 1048576) -> bool:
     print(f'send @\tERR @\tFunction is developing...')
 def receive(receive_socket : socket, filepath : str) -> bool:
     print(f'receive @\tERR @\tFunction is developing...')
