18aa0dc0a0344beaec9a15e93d4da3245e378c02
nickhuy1809 <caotanhoanghuy@gmail.com>
2024-07-20 14:07:52 +0700

    FileSpiltter

---
+++ file_splitter.py
@@ -0,0 +1,23 @@
+import os
+
+def split_file(filepath, chunk_maxsize=1048576):
+    part_num = 0
+    with open(filepath, 'rb') as file:
+        while True:
+            chunk = file.read(chunk_maxsize)
+            if not chunk:
+                break
+            with open(f"{filepath}.part{part_num}", 'wb') as chunk_file:
+                chunk_file.write(chunk)
+            part_num += 1
+    return part_num
+
+def merge_file(filepath, part_num):
+    with open(filepath, 'wb') as output_file:
+        for i in range(part_num):
+            part_filepath = f"{filepath}.part{i}"
+            if not os.path.exists(part_filepath):
+                return False
+            with open(part_filepath, 'rb') as part_file:
+                output_file.write(part_file.read())
+    return True
