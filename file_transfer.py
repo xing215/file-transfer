31bebc2155ef13777c25e98baa1591993db4a056
xing215 <dev@xing512.onmicrosoft.com>
2024-07-26 11:39:52 +0700

    Fix bug

--- file_transfer.py
+++ file_transfer.py
@@ -13,6 +13,8 @@ def send(send_socket: socket.socket, filepath: str, chunk_size: int = 1048576) -
         file_size = os.path.getsize(filepath)
         num_chunks_List = file_size // chunk_size + (1 if file_size % chunk_size != 0 else 0)
 
+        send_socket.send(str(num_chunks_List).encode())
+
         with open(filepath, 'rb') as f:
             threads = []
             for i in range(num_chunks_List):
@@ -23,39 +25,36 @@ def send(send_socket: socket.socket, filepath: str, chunk_size: int = 1048576) -
             
             for thread in threads:
                 thread.join()
-
-        send_chunk(send_socket, b'EOF')  
-
+        
         return True
     except Exception as e:
         print(f"send @\tERR @\t{e}")
         return False
 
-def receive_chunk(receive_socket: socket.socket, chunk_size: int, chunks_List: list, index: int):
+def receive_chunk(receive_socket: socket.socket, chunk_size: int, chunks_List: list):
     try:
-        chunks_List[index] = receive_socket.recv(chunk_size)
+        chunks_List.append(receive_socket.recv(chunk_size))
     except Exception as e:
         print(f"receive_chunk @\tERR @\t{e}")
 
 def receive(receive_socket: socket.socket, filepath: str, chunk_size: int = 1048576) -> bool:
     try:
+        num_chunks_List = int(receive_socket.recv(chunk_size).decode())
+        
         chunks_List = []
-        index = 0
-        while True:
-            chunks_List.append(None)
-            thread = threading.Thread(target=receive_chunk, args=(receive_socket, chunk_size, chunks_List, index))
+        threads = []
+        
+        for i in range(num_chunks_List):
+            thread = threading.Thread(target=receive_chunk, args=(receive_socket, chunk_size, chunks_List))
             thread.start()
-            thread.join()  # Cái này cần fix, tại này đang nhận được data nào là gộp vô luôn
-            if chunks_List[index] == b'EOF':
-                chunks_List.pop()  
-                break
-            index += 1
-
+            threads.append(thread)
+        for thr in threads:
+            thr.join()
         with open(filepath, 'wb') as file:
             for part in chunks_List:
                 if part:
-                    file.write(part)
                     file.flush()
+                    file.write(part)
         return True
     except Exception as e:
         print(f"receive @\tERR @\t{e}")
